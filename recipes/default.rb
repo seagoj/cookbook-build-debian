cce51c6d307083eb96403a163a1f57876b48f049
Jeremy Seago <seagoj@gmail.com>
2013-08-28 12:55:10 -0500

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,7 +5,7 @@ packages = %w(checkinstall automake build-essential make auto-apt)
 packages += JSON.parse(open("/vagrant/packages.json").read) if File.exists?("/vagrant/packages.json")
 packages.each{ |p| package p }
 
-builds = `cd /vagrant && ls *.tgz`+`cd /vagrant && ls *.tar.gz`+`cd /vagrant && ls *.bz2`
+builds = `ls /vagrant/*.tgz`+`ls /vagrant/*.tar.gz`+`ls /vagrant/*.bz2`
 puts builds
 
 %w(php-5.5.2 php-5.5.2.tar.bz2 dependencies php_5.5.2-1.debian.tar.gz).each do |f|
