c3092329a07b2c772b08da83497abc2503f14b81
Jeremy Seago <seagoj@gmail.com>
2013-08-29 18:44:02 -0500

    Update php-5.5.2.rb

--- recipes/php-5.5.2.rb
+++ recipes/php-5.5.2.rb
@@ -1,3 +1,4 @@
 %w(bison libxml2 libxml2-dev libcurl3 libcurl4-gnutls-dev libmagic-dev curl).each { package p }
 
 config = "--prefix=/usr --sysconfdir=/etc --with-config-file-path=/etc --enable-fpm --with-fpm-user=www-data --with-fpm-group=www-data --enable-opcache --enable-mbstring --enable-mbregex --enable-zip --with-mysqli --with-openssl --with-curl --with-zlib --enable-pcntl"
+source = ""
