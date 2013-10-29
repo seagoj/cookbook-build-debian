e691c2e1535678cb90e7dddbc4aa81222a9da3e5
jeremy Seago <jseago@bellevuerx.com>
2013-10-29 16:20:42 -0500

    Adds metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name "build-debian"
+description "Builds debian files from source using specified configurations"
+version "0.1"
+depends "apt"
+depends "ruby"
+
+recipe "build-debian", "Builds debian files from source using specified configurations"
