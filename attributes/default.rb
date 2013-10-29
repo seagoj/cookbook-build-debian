a94f5fffb1d09fe8b986fbdee96a210034c0040f
Jeremy Seago <seagoj@gmail.com>
2013-10-29 12:57:13 -0500

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
 default[:build_debian][:user] = "root"
 default[:build_debian][:group] = "root"
-default[:build_debian][:php-5.5.2_config] = "--prefix=/usr --sysconfdir=/etc --with-config-file-path=/etc --enable-fpm --with-fpm-user=www-data --with-fpm-group=www-data --enable-opcache --enable-mbstring --enable-mbregex --enable-zip --with-mysqli --with-openssl --with-curl --with-zlib --enable-pcntl"
+default[:build_debian][:php-5.5.2_config] = "--prefix=/usr --sysconfdir=/etc --with-config-file-path=/etc --enable-fpm --with-fpm-user=www-data --with-fpm-group=www-data --with-interbase=/etc/firebird --enable-opcache --enable-mbstring --enable-mbregex --enable-zip --with-mysqli --with-openssl --with-curl --with-zlib --enable-pcntl"
 default[:build_debian][:php-5.5.2_source] = "http://www.php.net/get/php-5.5.2.tar.bz2/from/us1.php.net/mirror"
